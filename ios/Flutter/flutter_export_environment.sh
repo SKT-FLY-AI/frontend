07e8f83973daa119fedf1515608325c5be6d67d7
Hyun <105445917+Kwon-Hyun@users.noreply.github.com>
2024-08-20 12:14:41 +0900

    Add files via upload

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/hyunowk/Downloads/flutter_poop"
-export "FLUTTER_APPLICATION_PATH=/Users/hyunowk/Downloads/poopsee_hyun"
+export "FLUTTER_ROOT=/Users/hyunowk/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hyunowk/Downloads/Flutter_Project/poopsee_hyun"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
