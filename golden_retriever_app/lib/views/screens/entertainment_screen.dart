860b884f4c2772e5dad234df597bdb9fd146aaf1
Chaeny Han <hxchaeny@gmail.com>
2024-08-21 02:17:25 +0900

    Design: 엔터 페이지 글라스모피즘 적용

--- golden_retriever_app/lib/views/screens/entertainment_screen.dart
+++ golden_retriever_app/lib/views/screens/entertainment_screen.dart
@@ -1,276 +1,222 @@
-// // lib/screens/entertainment_screen.dart
-//
-// import 'package:flutter/cupertino.dart';
-// import '../widgets/custom_bottom_navigation_bar.dart';
-//
-// class EntertainmentScreen extends StatefulWidget {
-//   const EntertainmentScreen({
-//     super.key,
-//   });
-//
-//   @override
-//   _EntertainmentScreen createState() => _EntertainmentScreen();
-// }
-//
-// class _EntertainmentScreen extends State<EntertainmentScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     final double contextHeight = MediaQuery.of(context).size.height * 0.1;
-//     final double contextWidth = MediaQuery.of(context).size.width * 0.1;
-//
-//     return CupertinoPageScaffold(
-//       child: SafeArea(
-//         child: Stack(
-//           children: [
-//             Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 SizedBox(height: contextHeight * 0.5,),
-//                 Column(
-//                   mainAxisSize: MainAxisSize.min,
-//                   children: [
-//                     Text(
-//                       "YouTube 추천 영상",
-//                       style: TextStyle(
-//                         fontSize: contextHeight * 0.25,
-//                         fontWeight: FontWeight.bold,
-//                         color: CupertinoColors.white,
-//                         decoration: TextDecoration.none, // 밑줄 제거
-//                       ),
-//                     ),
-//                     ListView(
-//                       children: [
-//                         CupertinoListTile(
-//                           title: Text(
-//                             "추천 영상 1",
-//                             style: TextStyle(color: CupertinoColors.black),
-//                           ),
-//                         ),
-//                         CupertinoListTile(
-//                           title: Text(
-//                             "추천 영상 2",
-//                             style: TextStyle(color: CupertinoColors.black),
-//                           ),
-//                         ),
-//                         CupertinoListTile(
-//                           title: Text(
-//                             "추천 영상 3",
-//                             style: TextStyle(color: CupertinoColors.black),
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ],
-//                 ),
-//                 Column(
-//                   mainAxisSize: MainAxisSize.min,
-//                   children: [
-//                     Text(
-//                       "건강 뉴스",
-//                       style: TextStyle(
-//                         fontSize: contextHeight * 0.25,
-//                         fontWeight: FontWeight.bold,
-//                         color: CupertinoColors.white,
-//                         decoration: TextDecoration.none, // 밑줄 제거
-//                       ),
-//                     ),
-//                     ListView(
-//                       children: [
-//                         CupertinoListTile(
-//                           title: Text(
-//                             "건강 뉴스 1",
-//                             style: TextStyle(color: CupertinoColors.black),
-//                           ),
-//                         ),
-//                         CupertinoListTile(
-//                           title: Text(
-//                             "건강 뉴스 2",
-//                             style: TextStyle(color: CupertinoColors.black),
-//                           ),
-//                         ),
-//                         CupertinoListTile(
-//                           title: Text(
-//                             "건강 뉴스 3",
-//                             style: TextStyle(color: CupertinoColors.black),
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ]
-//                 ),
-//                 Column(
-//                   mainAxisSize: MainAxisSize.min,
-//                   children: [
-//                     Text(
-//                       "PoopSee News",
-//                       style: TextStyle(
-//                         fontSize: contextHeight * 0.25,
-//                         fontWeight: FontWeight.bold,
-//                         color: CupertinoColors.white,
-//                         decoration: TextDecoration.none, // 밑줄 제거
-//                       ),
-//                     ),
-//                     ListView(
-//                       children: [
-//                         CupertinoListTile(
-//                           title: Text(
-//                             "뉴스 1",
-//                             style: TextStyle(color: CupertinoColors.black),
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   ]
-//                 ),
-//               ],
-//             ),
-//             Positioned(
-//               left: 0,
-//               right: 0,
-//               bottom: 0,
-//               child: CustomBottomNavigationBar(
-//                 currentIndex: 3,
-//                 onTap: (currentIndex) {
-//                   // CustomBottomNavigationBar의 네비게이션 핸들러는 이미 정의되어 있으므로 추가 작업 불필요
-//                 },
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// class CupertinoListTile extends StatelessWidget {
-//   final Widget title;
-//   final Function()? onTap;
-//
-//   CupertinoListTile({
-//     required this.title,
-//     this.onTap,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final double contextWidth = MediaQuery.of(context).size.width * 0.1;
-//
-//     return CupertinoButton(
-//       padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: contextWidth),
-//       onPressed: onTap,
-//       child: Align(
-//         alignment: Alignment.centerLeft,
-//         child: title,
-//       ),
-//     );
-//   }
-// }
-
 // lib/screens/entertainment_screen.dart
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import '../widgets/custom_bottom_navigation_bar.dart';
 
-class EntertainmentScreen extends StatefulWidget {
-  const EntertainmentScreen({
-    super.key,
-  });
+class EntertainmentScreen extends StatelessWidget {
+  const EntertainmentScreen({super.key});
 
-  @override
-  _EntertainmentScreen createState() => _EntertainmentScreen();
-}
-
-class _EntertainmentScreen extends State<EntertainmentScreen> {
   @override
   Widget build(BuildContext context) {
     final double contextHeight = MediaQuery.of(context).size.height * 0.1;
     final double contextWidth = MediaQuery.of(context).size.width * 0.1;
 
     return CupertinoPageScaffold(
-      child: SafeArea(
-        child: Stack(
-          children: [
-            Column(
+      navigationBar: CupertinoNavigationBar(
+        border: null, // 테두리 없음
+        backgroundColor: CupertinoColors.transparent, // 상단바 색상 없음
+      ),
+      backgroundColor: Colors.orangeAccent,
+      child: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: contextWidth * 0.8),
+            child: ListView(
               children: [
-                SizedBox(height: contextHeight * 0.5,),
-                Text("YouTube 추천 영상"),
-                Expanded(
-                  child: ListView(
-                    children: [
-                      CupertinoListTile(
-                        title: Text(
-                          "추천 영상 1",
-                          style: TextStyle(color: CupertinoColors.black),
-                        ),
-                      ),
-                      CupertinoListTile(
-                        title: Text(
-                          "추천 영상 2",
-                          style: TextStyle(color: CupertinoColors.black),
-                        ),
-                      ),
-                      CupertinoListTile(
-                        title: Text(
-                          "추천 영상 3",
-                          style: TextStyle(color: CupertinoColors.black),
-                        ),
-                      ),
-                    ],
+                // YouTube 추천 영상 섹션
+                Padding(
+                  padding: EdgeInsets.only(top: contextHeight * 0.2, bottom: contextHeight * 0.15),
+                  child: Text(
+                    "YouTube 추천 영상",
+                    style: TextStyle(
+                      fontSize: contextWidth * 0.45,
+                      color: CupertinoColors.white,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none, // 밑줄 제거
+                    ),
                   ),
                 ),
-                Text("건강 뉴스"),
-                Expanded(
-                  child: ListView(
-                    children: [
-                      CupertinoListTile(
-                        title: Text(
-                          "건강 뉴스 1",
-                          style: TextStyle(color: CupertinoColors.black),
+                Container(
+                  padding: EdgeInsets.all(16.0), // 내부 여백
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.3), // 반투명 배경색
+                    borderRadius: BorderRadius.circular(16.0), // 모서리 둥글게
+                    border: Border.all(
+                      color: Colors.white.withOpacity(0.5), // 테두리 색상
+                      width: 1.0,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: contextWidth * 0.2,
+                      vertical: contextHeight * 0.15
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: contextWidth * 0.2),
+                              child: Image.asset('assets/images/poop_great.png', height: contextHeight * 1),
+                            ),
+                            SizedBox(width: contextWidth * 0.3,),
+                            Text(
+                              "영상 제목",
+                              style: TextStyle(
+                                fontSize: contextWidth * 0.4,
+                                fontWeight: FontWeight.w600,
+                                color: CupertinoColors.white, // 텍스트 색상
+                                decoration: TextDecoration.none, // 밑줄 제거
+                              )
+                            ),
+                          ],
                         ),
-                      ),
-                      CupertinoListTile(
-                        title: Text(
-                          "건강 뉴스 2",
-                          style: TextStyle(color: CupertinoColors.black),
+                        SizedBox(height: contextHeight * 0.1,),
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: contextWidth * 0.2),
+                              child: Image.asset('assets/images/poop_great.png', height: contextHeight * 1),
+                            ),
+                            SizedBox(width: contextWidth * 0.3,),
+                            Text(
+                                "영상 제목",
+                                style: TextStyle(
+                                  fontSize: contextWidth * 0.4,
+                                  fontWeight: FontWeight.w600,
+                                  color: CupertinoColors.white, // 텍스트 색상
+                                  decoration: TextDecoration.none, // 밑줄 제거
+                                )
+                            ),
+                          ],
                         ),
-                      ),
-                      CupertinoListTile(
-                        title: Text(
-                          "건강 뉴스 3",
-                          style: TextStyle(color: CupertinoColors.black),
+                        SizedBox(height: contextHeight * 0.1,),
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: contextWidth * 0.2),
+                              child: Image.asset('assets/images/poop_great.png', height: contextHeight * 1),
+                            ),
+                            SizedBox(width: contextWidth * 0.3,),
+                            Text(
+                                "영상 제목",
+                                style: TextStyle(
+                                  fontSize: contextWidth * 0.4,
+                                  fontWeight: FontWeight.w600,
+                                  color: CupertinoColors.white, // 텍스트 색상
+                                  decoration: TextDecoration.none, // 밑줄 제거
+                                )
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-                Text("PoopSee News"),
-                Expanded(
-                  child: ListView(
-                    children: [
-                      CupertinoListTile(
-                        title: Text(
-                          "뉴스 1",
-                          style: TextStyle(color: CupertinoColors.black),
-                        ),
-                      ),
-                    ],
+                // 건강 뉴스 섹션
+                Padding(
+                  padding: EdgeInsets.only(top: contextHeight * 0.3, bottom: contextHeight * 0.15),
+                  child: Text(
+                    "건강 뉴스",
+                    style: TextStyle(
+                      fontSize: contextWidth * 0.45,
+                      color: CupertinoColors.white,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none, // 밑줄 제거
+                    ),
                   ),
                 ),
+                Container(
+                    padding: EdgeInsets.all(16.0), // 내부 여백
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.3), // 반투명 배경색
+                      borderRadius: BorderRadius.circular(16.0), // 모서리 둥글게
+                      border: Border.all(
+                        color: Colors.white.withOpacity(0.5), // 테두리 색상
+                        width: 1.0,
+                      ),
+                    ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: contextWidth * 0.2,
+                        vertical: contextHeight * 0.15
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CupertinoListTile(
+                          title: Text(
+                            "건강 뉴스 1",
+                            style: TextStyle(color: CupertinoColors.white),
+                          ),
+                        ),
+                        SizedBox(height: contextHeight * 0.15,),
+                        CupertinoListTile(
+                          title: Text(
+                            "건강 뉴스 2",
+                            style: TextStyle(color: CupertinoColors.white),
+                          ),
+                        ),
+                        SizedBox(height: contextHeight * 0.15,),
+                        CupertinoListTile(
+                          title: Text(
+                            "건강 뉴스 3",
+                            style: TextStyle(color: CupertinoColors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ),
+
+                // PoopSee News 섹션
+                Padding(
+                  padding: EdgeInsets.only(top: contextHeight * 0.3, bottom: contextHeight * 0.15),
+                  child: Text(
+                    "PoopSee News",
+                    style: TextStyle(
+                      fontSize: contextWidth * 0.45,
+                      color: CupertinoColors.white,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none, // 밑줄 제거
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(16.0), // 내부 여백
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.3), // 반투명 배경색
+                    borderRadius: BorderRadius.circular(16.0), // 모서리 둥글게
+                    border: Border.all(
+                      color: Colors.white.withOpacity(0.5), // 테두리 색상
+                      width: 1.0,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: contextWidth * 0.2,
+                        vertical: contextHeight * 0.15
+                    ),
+                    child: SizedBox(height: contextHeight,),
+                  ),
+                ),
+                SizedBox(height: contextHeight * 3,),
               ],
             ),
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: CustomBottomNavigationBar(
-                currentIndex: 3,
-                onTap: (currentIndex) {
-                  // CustomBottomNavigationBar의 네비게이션 핸들러는 이미 정의되어 있으므로 추가 작업 불필요
-                },
-              ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: CustomBottomNavigationBar(
+              currentIndex: 3,
+              onTap: (currentIndex) {
+                // CustomBottomNavigationBar의 네비게이션 핸들러는 이미 정의되어 있으므로 추가 작업 불필요
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -287,10 +233,11 @@ class CupertinoListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double contextHeight = MediaQuery.of(context).size.height * 0.1;
     final double contextWidth = MediaQuery.of(context).size.width * 0.1;
 
     return CupertinoButton(
-      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: contextWidth),
+      padding: EdgeInsets.symmetric(vertical: contextHeight * 0.1, horizontal: contextWidth * 0.05),
       onPressed: onTap,
       child: Align(
         alignment: Alignment.centerLeft,
